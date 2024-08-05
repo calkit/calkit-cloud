da3094be880fa3ab89314c2f07087576931c5194
Pete Bachant <petebachant@gmail.com>
2024-08-05 16:39:26 -0700

    Remove sidebar import

--- frontend/src/routes/_layout.tsx
+++ frontend/src/routes/_layout.tsx
@@ -1,7 +1,6 @@
 import { Flex, Spinner } from "@chakra-ui/react"
 import { Outlet, createFileRoute, redirect } from "@tanstack/react-router"
 
-import Sidebar from "../components/Common/Sidebar"
 import UserMenu from "../components/Common/UserMenu"
 import useAuth, { isLoggedIn } from "../hooks/useAuth"
 
