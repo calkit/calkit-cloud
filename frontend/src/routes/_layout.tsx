40830c41bc947e1c64e240466a02d78568b9288a
Pete Bachant <petebachant@gmail.com>
2024-08-05 17:25:30 -0700

    Add home icon link

--- frontend/src/routes/_layout.tsx
+++ frontend/src/routes/_layout.tsx
@@ -1,6 +1,6 @@
-import { Flex, Spinner } from "@chakra-ui/react"
+import { Flex, Spinner, Box, Link, Image } from "@chakra-ui/react"
 import { Outlet, createFileRoute, redirect } from "@tanstack/react-router"
-
+import { Link as RouterLink } from "@tanstack/react-router"
 import UserMenu from "../components/Common/UserMenu"
 import useAuth, { isLoggedIn } from "../hooks/useAuth"
 
@@ -20,6 +20,22 @@ function Layout() {
 
   return (
     <Flex maxW="large" h="auto" position="relative">
+      <Box
+        display="block"
+        position="relative"
+        top={2}
+        left={6}
+        alignItems="baseline"
+        h={20}
+      >
+        <Link as={RouterLink} to="/">
+          <Image
+            width={10}
+            src="/public/assets/images/kdot.svg"
+            alt="Home"
+          ></Image>
+        </Link>
+      </Box>
       {isLoading ? (
         <Flex justify="center" align="center" height="100vh" width="full">
           <Spinner size="xl" color="ui.main" />
