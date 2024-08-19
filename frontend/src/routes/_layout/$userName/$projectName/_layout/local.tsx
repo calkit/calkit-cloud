99197a5aef80ec167af9a52cac8a13b49f052e4f
Pete Bachant <petebachant@gmail.com>
2024-08-19 13:38:07 -0700

    Add CWD to local machine page

--- frontend/src/routes/_layout/$userName/$projectName/_layout/local.tsx
+++ frontend/src/routes/_layout/$userName/$projectName/_layout/local.tsx
@@ -1,5 +1,7 @@
-import { Code, Box } from "@chakra-ui/react"
+import { Code, Box, Text, Heading } from "@chakra-ui/react"
+import { useQuery } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
+import axios from "axios"
 
 export const Route = createFileRoute(
   "/_layout/$userName/$projectName/_layout/local",
@@ -8,11 +10,24 @@ export const Route = createFileRoute(
 })
 
 function LocalServer() {
+  const cwdQuery = useQuery({
+    queryKey: ["local-cwd"],
+    queryFn: () => axios.get("http://localhost:8866/cwd"),
+  })
+
   return (
     <>
       <Box>
-        To connect your local machine, run <Code>calkit server</Code> in a
-        terminal.
+        <Heading size="md" mb={1}>
+          Local machine
+        </Heading>
+        <Text>
+          Current working directory: <Code>{cwdQuery?.data?.data}</Code>
+        </Text>
+        <Text>
+          To connect your local machine, run <Code>calkit server</Code> in a
+          terminal.
+        </Text>
       </Box>
     </>
   )
