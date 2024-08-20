1d1563d1d04e095314c30824c2e4174a7528d4d0
Pete Bachant <petebachant@gmail.com>
2024-08-20 16:35:18 -0700

    Some work on local server page

--- frontend/src/routes/_layout/$userName/$projectName/_layout/local.tsx
+++ frontend/src/routes/_layout/$userName/$projectName/_layout/local.tsx
@@ -1,7 +1,17 @@
-import { Code, Box, Text, Heading } from "@chakra-ui/react"
+import {
+  Code,
+  Box,
+  Text,
+  Heading,
+  Button,
+  Spinner,
+  Flex,
+  Icon,
+} from "@chakra-ui/react"
 import { useQuery } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
 import axios from "axios"
+import { FiExternalLink } from "react-icons/fi"
 
 export const Route = createFileRoute(
   "/_layout/$userName/$projectName/_layout/local",
@@ -13,7 +23,13 @@ function LocalServer() {
   const cwdQuery = useQuery({
     queryKey: ["local-cwd"],
     queryFn: () => axios.get("http://localhost:8866/cwd"),
+    retry: false,
   })
+  // TODO: We should be sending some information about the project so we open
+  // the correct directory
+  const openVSCode = () => {
+    axios.post("http://localhost:8866/open/vscode")
+  }
 
   return (
     <>
@@ -21,13 +37,31 @@ function LocalServer() {
         <Heading size="md" mb={1}>
           Local machine
         </Heading>
-        <Text>
-          Current working directory: <Code>{cwdQuery?.data?.data}</Code>
-        </Text>
-        <Text>
-          To connect your local machine, run <Code>calkit server</Code> in a
-          terminal.
-        </Text>
+        {cwdQuery.isPending ? (
+          <Flex justify="center" align="center" height="100vh" width="full">
+            <Spinner size="xl" color="ui.main" />
+          </Flex>
+        ) : (
+          <Flex>
+            {!cwdQuery.error ? (
+              <Box>
+                <Text>
+                  Current working directory: <Code>{cwdQuery?.data?.data}</Code>
+                </Text>
+                <Button variant="primary" onClick={openVSCode}>
+                  Open in VSCode <Icon ml={1} as={FiExternalLink} />
+                </Button>
+              </Box>
+            ) : (
+              <Box>
+                <Text>
+                  Local server not connected. To connect your local machine, run{" "}
+                  <Code>calkit server</Code> in a terminal.
+                </Text>
+              </Box>
+            )}
+          </Flex>
+        )}
       </Box>
     </>
   )
