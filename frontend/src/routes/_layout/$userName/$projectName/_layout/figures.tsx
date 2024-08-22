cc212b065f4a16231da9b0ced3a835578730b378
Pete Bachant <petebachant@gmail.com>
2024-08-22 12:19:54 -0700

    Start figure comments component

--- frontend/src/routes/_layout/$userName/$projectName/_layout/figures.tsx
+++ frontend/src/routes/_layout/$userName/$projectName/_layout/figures.tsx
@@ -1,14 +1,65 @@
-import { Box, Heading, Spinner, Flex, Text, Code } from "@chakra-ui/react"
+import {
+  Box,
+  Heading,
+  Spinner,
+  Flex,
+  Text,
+  Textarea,
+  Button,
+} from "@chakra-ui/react"
 import { useQuery } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
+import { useState } from "react"
 
-import { ProjectsService } from "../../../../../client"
+import { ProjectsService, type Figure } from "../../../../../client"
 
 export const Route = createFileRoute(
   "/_layout/$userName/$projectName/_layout/figures",
 )({
   component: ProjectFigures,
 })
+
+interface CommentsProps {
+  figure: Figure
+}
+
+function FigureComments({ figure }: CommentsProps) {
+  const [commentInput, setCommentInput] = useState("")
+  const handleInputChange = (val) => {
+    setCommentInput(val.target.value)
+  }
+  const onButtonClick = (e) => {
+    console.log("Button clicked on", figure.path)
+  }
+
+  return (
+    <>
+      <Heading size="s" mb={2}>
+        Comments
+      </Heading>
+      <Box>
+        <Text>Someone (time): This is my comment.</Text>
+        <Text>Else (time): This is my comment.</Text>
+        <Textarea
+          mt={2}
+          value={commentInput}
+          onChange={handleInputChange}
+          placeholder="Add a comment"
+        />
+        <Flex justifyItems={"end"} justifyContent={"end"}>
+          <Button
+            id={figure.path}
+            my={2}
+            isDisabled={commentInput === ""}
+            onClick={onButtonClick}
+          >
+            Submit
+          </Button>
+        </Flex>
+      </Box>
+    </>
+  )
+}
 
 function ProjectFiguresView() {
   const { userName, projectName } = Route.useParams()
@@ -36,13 +87,18 @@ function ProjectFiguresView() {
               </Heading>
               <Text>{figure.description}</Text>
               {figure.path.endsWith(".pdf") && figure.content ? (
-                <Box height="530px" width="635px" my={3}>
-                  <embed
-                    height="100%"
-                    width="100%"
-                    src={`data:application/pdf;base64,${figure.content}`}
-                  />
-                </Box>
+                <Flex my={3}>
+                  <Box height="530px" width="635px">
+                    <embed
+                      height="100%"
+                      width="100%"
+                      src={`data:application/pdf;base64,${figure.content}`}
+                    />
+                  </Box>
+                  <Box mx={4} width={"50%"}>
+                    <FigureComments figure={figure} />
+                  </Box>
+                </Flex>
               ) : (
                 "Cannot render figure content."
               )}
