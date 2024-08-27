7a01a7a648d07b1ed73c536da3a211370800c38b
Pete Bachant <petebachant@gmail.com>
2024-08-27 13:42:19 -0700

    Handle projects with empty workflows

--- frontend/src/routes/_layout/$userName/$projectName/_layout/workflow.tsx
+++ frontend/src/routes/_layout/$userName/$projectName/_layout/workflow.tsx
@@ -1,5 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router"
-import { Box, Code, Flex, Spinner, Heading } from "@chakra-ui/react"
+import {
+  Box,
+  Code,
+  Flex,
+  Spinner,
+  Heading,
+  Alert,
+  AlertIcon,
+} from "@chakra-ui/react"
 import { useQuery } from "@tanstack/react-query"
 
 import Mermaid from "../../../../../components/Common/Mermaid"
@@ -35,24 +43,34 @@ function ProjectWorkflow() {
         </Flex>
       ) : (
         <Flex>
-          <Box p={5} maxW="50%" minW="40%">
-            <Mermaid>{String(workflowQuery?.data?.mermaid)}</Mermaid>
-          </Box>
-          <Box width="680px">
-            <Heading size="md" my={2}>
-              YAML
-            </Heading>
-            <Code
-              p={2}
-              borderRadius={"lg"}
-              display="block"
-              whiteSpace="pre"
-              height="78vh"
-              overflowY="auto"
-            >
-              {String(workflowQuery?.data?.yaml)}
-            </Code>
-          </Box>
+          {workflowQuery.data ? (
+            <>
+              <Box p={5} maxW="50%" minW="40%">
+                <Mermaid>{String(workflowQuery?.data?.mermaid)}</Mermaid>
+              </Box>
+              <Box width="680px">
+                <Heading size="md" my={2}>
+                  YAML
+                </Heading>
+                <Code
+                  p={2}
+                  borderRadius={"lg"}
+                  display="block"
+                  whiteSpace="pre"
+                  height="78vh"
+                  overflowY="auto"
+                >
+                  {String(workflowQuery?.data?.yaml)}
+                </Code>
+              </Box>
+            </>
+          ) : (
+            <Alert mt={2} status="warning" borderRadius="xl">
+              <AlertIcon />A workflow has not yet been defined for this project.
+              To create one, add stages to the <Code mx={1}>dvc.yaml</Code>{" "}
+              file.
+            </Alert>
+          )}
         </Flex>
       )}
     </>
