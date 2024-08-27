b5a057105492613d32c736e46cfaec5222aa3ec7
Pete Bachant <petebachant@gmail.com>
2024-08-27 11:24:54 -0700

    Make topbar sticky

--- frontend/src/components/Common/Topbar.tsx
+++ frontend/src/components/Common/Topbar.tsx
@@ -53,7 +53,7 @@ export default function Topbar() {
 
   return (
     <>
-      <Box bg={secBgColor} px={4}>
+      <Box bg={secBgColor} px={4} position={"sticky"} top={0} zIndex={1000}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
           <IconButton
             size={"md"}
