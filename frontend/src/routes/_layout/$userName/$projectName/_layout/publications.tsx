aff5521549c0802afe58b36e1624b52c23caa430
Pete Bachant <petebachant@gmail.com>
2024-08-31 07:56:31 -0700

    Add query for publications

--- frontend/src/routes/_layout/$userName/$projectName/_layout/publications.tsx
+++ frontend/src/routes/_layout/$userName/$projectName/_layout/publications.tsx
@@ -12,10 +12,14 @@ import {
   MenuList,
   MenuItem,
   useDisclosure,
+  Spinner,
 } from "@chakra-ui/react"
 import { createFileRoute } from "@tanstack/react-router"
+import { useQuery } from "@tanstack/react-query"
 import { FiFile } from "react-icons/fi"
 import { FaPlus } from "react-icons/fa"
+
+import { ProjectsService } from "../../../../../client"
 
 export const Route = createFileRoute(
   "/_layout/$userName/$projectName/_layout/publications",
@@ -27,57 +31,76 @@ function Publications() {
   const secBgColor = useColorModeValue("ui.secondary", "ui.darkSlate")
   const uploadPubModal = useDisclosure()
   const labelPubModal = useDisclosure()
+  const { userName, projectName } = Route.useParams()
+  const pubsQuery = useQuery({
+    queryKey: ["projects", userName, projectName, "publications"],
+    queryFn: () =>
+      ProjectsService.getProjectPublications({
+        ownerName: userName,
+        projectName: projectName,
+      }),
+  })
 
   return (
-    <Flex>
-      {/* A nav bar at the left with a heading, upload menu and list of pubs */}
-      <Box
-        minW={"200px"}
-        bg={secBgColor}
-        borderRadius={"lg"}
-        px={3}
-        py={2}
-        mr={4}
-        position={"sticky"}
-        top="55"
-      >
-        <Flex align={"center"} mb={1}>
-          <Heading size={"md"}>Publications</Heading>
-          <Menu>
-            <MenuButton>
-              <Button height={"25px"} width={"9px"} px={0.5} ml={2}>
-                <Icon as={FaPlus} fontSize={"xs"} />
-              </Button>
-            </MenuButton>
-            <MenuList>
-              <MenuItem onClick={uploadPubModal.onOpen}>
-                Upload new publication
-              </MenuItem>
-              <MenuItem onClick={labelPubModal.onOpen}>
-                Label existing file as publication
-              </MenuItem>
-            </MenuList>
-          </Menu>
+    <>
+      {pubsQuery.isPending ? (
+        <Flex justify="center" align="center" height="100vh" width="full">
+          <Spinner size="xl" color="ui.main" />
         </Flex>
-        {/* Iterate over all publications to create an anchor link for each */}
-        <Link>
-          <Text noOfLines={1}>
-            <Icon pt={1} as={FiFile} /> This is the name
-          </Text>
-        </Link>
-      </Box>
-      {/* A box to the right that iterates over all figures, adding a view for
-      the content, info, and comments */}
-      <Box width={"100%"}>
+      ) : (
         <Flex>
-          <Box width={"66%"}>This is the publication content view</Box>
+          {/* A nav bar at the left with a heading, upload menu and list of
+           pubs */}
+          <Box
+            minW={"200px"}
+            bg={secBgColor}
+            borderRadius={"lg"}
+            px={3}
+            py={2}
+            mr={4}
+            position={"sticky"}
+            top="55"
+          >
+            <Flex align={"center"} mb={1}>
+              <Heading size={"md"}>Publications</Heading>
+              <Menu>
+                <MenuButton>
+                  <Button height={"25px"} width={"9px"} px={0.5} ml={2}>
+                    <Icon as={FaPlus} fontSize={"xs"} />
+                  </Button>
+                </MenuButton>
+                <MenuList>
+                  <MenuItem onClick={uploadPubModal.onOpen}>
+                    Upload new publication
+                  </MenuItem>
+                  <MenuItem onClick={labelPubModal.onOpen}>
+                    Label existing file as publication
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            </Flex>
+            {/* Iterate over all publications to create an anchor link for
+             each */}
+            <Link>
+              <Text noOfLines={1}>
+                <Icon pt={1} as={FiFile} /> This is the name
+              </Text>
+            </Link>
+          </Box>
+          {/* A box to the right that iterates over all figures, adding a view
+           for the content, info, and comments */}
+          <Box width={"100%"}>
+            <Flex>
+              <Box width={"66%"}>This is the publication content view</Box>
 
-          <Box width={"33%"}>
-            <Box>This is the info view</Box>
-            <Box>This is the publication comments view</Box>
+              <Box width={"33%"}>
+                <Box>This is the info view</Box>
+                <Box>This is the publication comments view</Box>
+              </Box>
+            </Flex>
           </Box>
         </Flex>
-      </Box>
-    </Flex>
+      )}
+    </>
   )
 }
