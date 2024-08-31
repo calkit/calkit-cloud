4849dec8ff87a5cf647cef37f3038557cd6df448
Pete Bachant <petebachant@gmail.com>
2024-08-31 11:58:17 -0700

    Put datasets in a grid layout

--- frontend/src/routes/_layout/$userName/$projectName/_layout/data.tsx
+++ frontend/src/routes/_layout/$userName/$projectName/_layout/data.tsx
@@ -8,6 +8,8 @@ import {
   ListItem,
   Code,
   Badge,
+  SimpleGrid,
+  Card,
 } from "@chakra-ui/react"
 import { useQuery } from "@tanstack/react-query"
 import { createFileRoute } from "@tanstack/react-router"
@@ -34,7 +36,7 @@ function ProjectDataView() {
   return (
     <>
       <Heading size="md" mb={2}>
-        Data
+        Datasets
       </Heading>
       {dataPending ? (
         <Flex justify="center" align="center" height="100vh" width="full">
@@ -42,24 +44,38 @@ function ProjectDataView() {
         </Flex>
       ) : (
         <Box>
-          <UnorderedList>
+          <SimpleGrid columns={[3, null, 4]} gap={6}>
             {datasets?.map((dataset) => (
-              <ListItem key={dataset.path}>
-                <Code>{dataset.path}</Code>
-                {dataset.imported_from ? (
-                  <Badge ml={1} bgColor="green.500">
-                    imported
-                  </Badge>
+              <Card key={dataset.path} p={6} variant="elevated">
+                <Heading size={"sm"} mb={2}>
+                  <Code p={1}>
+                    {dataset.path}
+                    {dataset.imported_from ? (
+                      <Badge ml={1} bgColor="green.500">
+                        imported
+                      </Badge>
+                    ) : (
+                      ""
+                    )}
+                  </Code>
+                </Heading>
+                {dataset.title ? (
+                  <Text mb={1}>
+                    <strong>Title:</strong> {dataset.title}
+                  </Text>
                 ) : (
                   ""
                 )}
-                <Text>Title: {dataset.title ? dataset.title : ""}</Text>
-                <Text>
-                  Description: {dataset.description ? dataset.description : ""}
-                </Text>
-              </ListItem>
+                {dataset.description ? (
+                  <Text>
+                    <strong>Description:</strong> {dataset.description}
+                  </Text>
+                ) : (
+                  ""
+                )}
+              </Card>
             ))}
-          </UnorderedList>
+          </SimpleGrid>
         </Box>
       )}
     </>
