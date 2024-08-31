a2b2c378de7b22f67f0657ce46bea0ec601584fb
Pete Bachant <petebachant@gmail.com>
2024-08-31 08:17:53 -0700

    Tweak layout of pubs page

--- frontend/src/routes/_layout/$userName/$projectName/_layout/publications.tsx
+++ frontend/src/routes/_layout/$userName/$projectName/_layout/publications.tsx
@@ -32,8 +32,10 @@ interface PubViewProps {
 }
 
 function PubView({ publication }: PubViewProps) {
+  const secBgColor = useColorModeValue("ui.secondary", "ui.darkSlate")
+
   return (
-    <Flex>
+    <Flex mb={2}>
       <Box width={"66%"}>
         <Heading size={"md"} id={publication.path}>
           {publication.title}
@@ -45,8 +47,14 @@ function PubView({ publication }: PubViewProps) {
       </Box>
 
       <Box width={"33%"}>
-        <Box>This is the info view</Box>
-        <Box>This is the publication comments view</Box>
+        <Box bg={secBgColor} borderRadius={"lg"} p={2} mb={2}>
+          <Heading size="sm">Info</Heading>
+          This is the info view
+        </Box>
+        <Box bg={secBgColor} borderRadius={"lg"} p={2}>
+          <Heading size="sm">Comments</Heading>
+          This is the publication comments view
+        </Box>
       </Box>
     </Flex>
   )
@@ -76,45 +84,47 @@ function Publications() {
         <Flex>
           {/* A nav bar at the left with a heading, upload menu and list of
            pubs */}
-          <Box
-            minW={"200px"}
-            bg={secBgColor}
-            borderRadius={"lg"}
-            px={3}
-            py={2}
-            mr={4}
-            position={"sticky"}
-            top="55"
-          >
-            <Flex align={"center"} mb={1}>
-              <Heading size={"md"}>Publications</Heading>
-              <Menu>
-                <MenuButton>
-                  <Button height={"25px"} width={"9px"} px={0.5} ml={2}>
-                    <Icon as={FaPlus} fontSize={"xs"} />
-                  </Button>
-                </MenuButton>
-                <MenuList>
-                  <MenuItem onClick={uploadPubModal.onOpen}>
-                    Upload new publication
-                  </MenuItem>
-                  <MenuItem onClick={labelPubModal.onOpen}>
-                    Label existing file as publication
-                  </MenuItem>
-                </MenuList>
-              </Menu>
-            </Flex>
-            {/* Iterate over all publications to create an anchor link for
+          <Box>
+            <Box
+              minW={"200px"}
+              bg={secBgColor}
+              borderRadius={"lg"}
+              px={3}
+              py={2}
+              mr={4}
+              position={"sticky"}
+              top="55"
+            >
+              <Flex align={"center"} mb={1}>
+                <Heading size={"md"}>Publications</Heading>
+                <Menu>
+                  <MenuButton>
+                    <Button height={"25px"} width={"9px"} px={0.5} ml={2}>
+                      <Icon as={FaPlus} fontSize={"xs"} />
+                    </Button>
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem onClick={uploadPubModal.onOpen}>
+                      Upload new publication
+                    </MenuItem>
+                    <MenuItem onClick={labelPubModal.onOpen}>
+                      Label existing file as publication
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </Flex>
+              {/* Iterate over all publications to create an anchor link for
              each */}
-            {pubsQuery.data
-              ? pubsQuery.data.map((pub) => (
-                  <Link key={pub.path} href={`#${pub.path}`}>
-                    <Text noOfLines={1}>
-                      <Icon pt={1} as={FiFile} /> {pub.title}
-                    </Text>
-                  </Link>
-                ))
-              : ""}
+              {pubsQuery.data
+                ? pubsQuery.data.map((pub) => (
+                    <Link key={pub.path} href={`#${pub.path}`}>
+                      <Text noOfLines={1}>
+                        <Icon pt={1} as={FiFile} /> {pub.title}
+                      </Text>
+                    </Link>
+                  ))
+                : ""}
+            </Box>
           </Box>
           {/* A box to the right that iterates over all figures, adding a view
            for the content, info, and comments */}
