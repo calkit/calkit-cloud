0e43dd7e48fb87f52e3708d67405d26045fe989b
Pete Bachant <petebachant@gmail.com>
2024-09-05 05:04:02 -0700

    Some work on references page

--- frontend/src/routes/_layout/$userName/$projectName/_layout/references.tsx
+++ frontend/src/routes/_layout/$userName/$projectName/_layout/references.tsx
@@ -1,9 +1,22 @@
+import { Box, Flex, Heading, useColorModeValue } from "@chakra-ui/react"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute(
   "/_layout/$userName/$projectName/_layout/references",
 )({
-  component: () => (
-    <div>Hello /_layout/$userName/$projectName/_layout/references!</div>
-  ),
+  component: References,
 })
+
+function References() {
+  const secBgColor = useColorModeValue("ui.secondary", "ui.darkSlate")
+
+  return (
+    <>
+      <Flex>
+        <Box bg={secBgColor} px={4} py={2} borderRadius="lg">
+          <Heading size="md">References</Heading>
+        </Box>
+      </Flex>
+    </>
+  )
+}
