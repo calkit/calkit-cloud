610ccb64d2a7206740a3e2eb2f14fb38bbdc3e3b
Pete Bachant <petebachant@gmail.com>
2024-09-05 19:01:17 -0700

    Some more work on references page

--- frontend/src/routes/_layout/$userName/$projectName/_layout/references.tsx
+++ frontend/src/routes/_layout/$userName/$projectName/_layout/references.tsx
@@ -10,8 +10,10 @@ import {
 import { createFileRoute } from "@tanstack/react-router"
 import { useQuery } from "@tanstack/react-query"
 import { IoLibraryOutline } from "react-icons/io5"
+import { FiFile } from "react-icons/fi"
 
 import { ProjectsService } from "../../../../../client"
+import { useState } from "react"
 
 export const Route = createFileRoute(
   "/_layout/$userName/$projectName/_layout/references",
@@ -25,7 +27,7 @@ function References() {
   const {
     isPending,
     error,
-    data: references,
+    data: allReferences,
   } = useQuery({
     queryKey: ["projects", userName, projectName, "References"],
     queryFn: () =>
@@ -34,6 +36,7 @@ function References() {
         projectName: projectName,
       }),
   })
+  const [selectedRefsIndex, setSelectedRefsIndex] = useState(0)
 
   return (
     <>
@@ -53,12 +56,28 @@ function References() {
                 <Heading size="md" mb={1}>
                   References
                 </Heading>
-                {references?.map((reference) => (
-                  <Box key={reference.path}>
-                    <Flex alignItems="center">
+                {allReferences?.map((references, index) => (
+                  <Box key={references.path}>
+                    <Flex
+                      alignItems="center"
+                      cursor="pointer"
+                      onClick={() => setSelectedRefsIndex(index)}
+                    >
                       <Icon mr={1} as={IoLibraryOutline} />
-                      <Text>{reference.path}</Text>
+                      <Text>{references.path}</Text>
                     </Flex>
+                    {index === selectedRefsIndex && references ? (
+                      <>
+                        {references.entries?.map((entry) => (
+                          <Flex ml={2} key={entry.key} alignItems="center">
+                            <Icon as={FiFile} mr={1} />
+                            <Text>{entry.key}</Text>
+                          </Flex>
+                        ))}
+                      </>
+                    ) : (
+                      ""
+                    )}
                   </Box>
                 ))}
               </Box>
