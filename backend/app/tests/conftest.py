fa00fd0877b7317eebe3e2d0f140fbcd194f74a1
Pete Bachant <petebachant@gmail.com>
2024-09-27 12:44:58 -0700

    Clean up tests

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 from app.config import settings
 from app.db import engine, init_db
 from app.main import app
-from app.models import Item, User
+from app.models import User
 from app.tests import (
     authentication_token_from_email,
     get_superuser_token_headers,
@@ -18,11 +18,6 @@ def db() -> Generator[Session, None, None]:
     with Session(engine) as session:
         init_db(session)
         yield session
-        statement = delete(Item)
-        session.execute(statement)
-        statement = delete(User)
-        session.execute(statement)
-        session.commit()
 
 
 @pytest.fixture(scope="session")
