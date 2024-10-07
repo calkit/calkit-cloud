66849716c6e19d63028588d88ef9491d666ea40f
abachant <aubreybachant@gmail.com>
2024-10-07 16:57:54 -0400

    Create basic PageMenu component

---
+++ frontend/src/components/Common/PageMenu.tsx
@@ -0,0 +1,27 @@
+import { Box } from "@chakra-ui/react"
+
+interface PageMenuProps {
+  children: React.ReactNode
+}
+
+const PageMenu = ({ children }: PageMenuProps) => {
+  return (
+    <Box
+      minW="200px"
+      maxW="300px"
+      overflowY="auto"
+      p={2}
+      mr={6}
+      mt={0}
+      borderRadius="lg"
+      bg="secBgColor"
+      borderWidth={0}
+      position="sticky"
+      top={55}
+    >
+      {children}
+    </Box>
+  )
+}
+
+export default PageMenu
