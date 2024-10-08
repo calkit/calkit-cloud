ec9fbc676988672464168ba9f9483a836a428e13
abachant <aubreybachant@gmail.com>
2024-10-08 12:36:20 -0400

    Fix color for border

--- frontend/src/components/Common/PageMenu.tsx
+++ frontend/src/components/Common/PageMenu.tsx
@@ -1,10 +1,11 @@
-import { Box } from "@chakra-ui/react"
+import { Box, useColorModeValue } from "@chakra-ui/react"
 
 interface PageMenuProps {
   children: React.ReactNode
 }
 
 const PageMenu = ({ children }: PageMenuProps) => {
+  const secBgColor = useColorModeValue("ui.secondary", "ui.darkSlate")
   return (
     <Box
       minW="200px"
@@ -14,7 +15,7 @@ const PageMenu = ({ children }: PageMenuProps) => {
       mr={6}
       mt={0}
       borderRadius="lg"
-      bg="secBgColor"
+      bg={secBgColor}
       borderWidth={0}
       position="sticky"
       top={55}
