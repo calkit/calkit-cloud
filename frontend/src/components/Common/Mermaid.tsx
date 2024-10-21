20446fb30d87aab6855b20a68f541e6120351a94
abachant <aubreybachant@gmail.com>
2024-10-21 17:56:44 -0400

    Add basic zooming functionality to mermaid svg figure

--- frontend/src/components/Common/Mermaid.tsx
+++ frontend/src/components/Common/Mermaid.tsx
@@ -1,11 +1,18 @@
 import { useEffect } from "react"
 import mermaid from "mermaid"
+import { zoom, zoomTransform } from "d3-zoom"
+import { select } from "d3-selection"
 
 interface MermaidProps {
   children: string
 }
 
 const Mermaid = ({ children }: MermaidProps) => {
+  const handleZoom = () => {
+    const transform = zoomTransform(select(".mermaid svg").node())
+    select(".mermaid svg g").attr("transform", transform)
+  }
+
   useEffect(() => {
     const renderDiagram = async () => {
       mermaid.initialize({
@@ -15,9 +22,14 @@ const Mermaid = ({ children }: MermaidProps) => {
         fontFamily: "monospace",
       })
       await mermaid.run({ querySelector: ".mermaid" })
+      select(".mermaid svg").call(
+        zoom().on("zoom", () => {
+          handleZoom()
+        }),
+      )
     }
     renderDiagram()
-  })
+  }, [])
   return <div className="mermaid">{children}</div>
 }
 
