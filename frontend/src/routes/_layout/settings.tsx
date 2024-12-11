2dc1eb1b7f04c3d036671efaca50aee9e2b6e956
Pete Bachant <petebachant@gmail.com>
2024-12-11 05:33:41 -0800

    Parse settings tab from query params

--- frontend/src/routes/_layout/settings.tsx
+++ frontend/src/routes/_layout/settings.tsx
@@ -8,7 +8,9 @@ import {
   Tabs,
 } from "@chakra-ui/react"
 import { useQueryClient } from "@tanstack/react-query"
-import { createFileRoute } from "@tanstack/react-router"
+import { createFileRoute, useNavigate } from "@tanstack/react-router"
+import { z } from "zod"
+import { useState } from "react"
 
 import type { UserPublic } from "../../client"
 import Appearance from "../../components/UserSettings/Appearance"
@@ -19,15 +21,18 @@ import UserTokens from "../../components/UserSettings/UserTokens"
 import { pageWidthNoSidebar } from "../../utils"
 
 const tabsConfig = [
-  { title: "My profile", component: UserInformation },
-  { title: "Password", component: ChangePassword },
-  { title: "Appearance", component: Appearance },
-  { title: "Tokens", component: UserTokens },
-  { title: "Danger zone", component: DeleteAccount },
+  { title: "My profile", component: UserInformation, slug: "profile" },
+  { title: "Password", component: ChangePassword, slug: "password" },
+  { title: "Appearance", component: Appearance, slug: "appearance" },
+  { title: "Tokens", component: UserTokens, slug: "tokens" },
+  { title: "Danger zone", component: DeleteAccount, slug: "delete-account" },
 ]
+
+const tabSearchSchema = z.object({ tab: z.string().catch("") })
 
 export const Route = createFileRoute("/_layout/settings")({
   component: UserSettings,
+  validateSearch: (search) => tabSearchSchema.parse(search),
 })
 
 function UserSettings() {
@@ -36,13 +41,33 @@ function UserSettings() {
   const finalTabs = currentUser?.is_superuser
     ? tabsConfig.slice(0, 4)
     : tabsConfig
+  const { tab } = Route.useSearch()
+  const navigate = useNavigate({ from: Route.fullPath })
+  let initialTabIndex = 0
+  if (tab) {
+    finalTabs.forEach((tabDef, index) => {
+      if (tab === tabDef.slug) {
+        initialTabIndex = index
+        console.log("Tab index", initialTabIndex, tabDef.slug)
+      }
+    })
+  }
+  const [activeTabIndex, setActiveTabIndex] = useState(initialTabIndex)
+  const handleTabChange = (index: number) => {
+    setActiveTabIndex(index)
+    navigate({ search: { tab: finalTabs[index].slug } })
+  }
 
   return (
     <Container maxW={pageWidthNoSidebar}>
       <Heading size="lg" textAlign={{ base: "center", md: "left" }} py={12}>
         Settings
       </Heading>
-      <Tabs variant="enclosed">
+      <Tabs
+        variant="enclosed"
+        index={activeTabIndex}
+        onChange={handleTabChange}
+      >
         <TabList>
           {finalTabs.map((tab, index) => (
             <Tab key={index}>{tab.title}</Tab>
