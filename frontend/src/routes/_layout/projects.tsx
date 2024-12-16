936c7dafa575d5458e9485c906c5a28728c58758
Pete Bachant <petebachant@gmail.com>
2024-12-16 11:32:34 -0800

    Create projects page

--- frontend/src/routes/_layout/projects.tsx
+++ frontend/src/routes/_layout/projects.tsx
@@ -1,19 +1,163 @@
-import { createFileRoute } from "@tanstack/react-router"
-import { Box, Container, Heading } from "@chakra-ui/react"
+import {
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Link,
+  SkeletonText,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react"
+import { ExternalLinkIcon } from "@chakra-ui/icons"
+import { useQuery, useQueryClient } from "@tanstack/react-query"
+import {
+  createFileRoute,
+  useNavigate,
+  Link as RouterLink,
+} from "@tanstack/react-router"
+import { z } from "zod"
+import { useEffect } from "react"
 
 import { pageWidthNoSidebar } from "../../utils"
+import { ProjectsService } from "../../client"
+
+const projectsSearchSchema = z.object({
+  page: z.number().catch(1),
+})
+
+const PER_PAGE = 20
+
+function getAllProjectsQueryOptions({ page }: { page: number }) {
+  return {
+    queryFn: () =>
+      ProjectsService.getProjects({
+        offset: (page - 1) * PER_PAGE,
+        limit: PER_PAGE,
+      }),
+    queryKey: ["projects-all", { page }],
+  }
+}
 
 export const Route = createFileRoute("/_layout/projects")({
   component: PublicProjects,
 })
 
+function PublicProjectsTable() {
+  const queryClient = useQueryClient()
+  const { page } = projectsSearchSchema.parse(Route.useSearch())
+  const navigate = useNavigate({ from: Route.fullPath })
+  const setPage = (page: number) =>
+    navigate({ search: (prev) => ({ ...prev, page }) })
+
+  const {
+    data: projects,
+    isPending,
+    isPlaceholderData,
+  } = useQuery({
+    ...getAllProjectsQueryOptions({ page }),
+    placeholderData: (prevData) => prevData,
+  })
+
+  const hasNextPage = !isPlaceholderData && projects?.data.length === PER_PAGE
+  const hasPreviousPage = page > 1
+
+  useEffect(() => {
+    if (hasNextPage) {
+      queryClient.prefetchQuery(getAllProjectsQueryOptions({ page: page + 1 }))
+    }
+  }, [page, queryClient, hasNextPage])
+
+  return (
+    <>
+      <TableContainer>
+        <Table size={{ base: "sm", md: "md" }}>
+          <Thead>
+            <Tr>
+              <Th>Owner</Th>
+              <Th>Title</Th>
+              <Th>GitHub URL</Th>
+              <Th>Description</Th>
+            </Tr>
+          </Thead>
+          {isPending ? (
+            <Tbody>
+              <Tr>
+                {new Array(4).fill(null).map((_, index) => (
+                  <Td key={index}>
+                    <SkeletonText noOfLines={1} paddingBlock="16px" />
+                  </Td>
+                ))}
+              </Tr>
+            </Tbody>
+          ) : (
+            <Tbody>
+              {projects?.data.map((project) => (
+                <Tr key={project.id} opacity={isPlaceholderData ? 0.5 : 1}>
+                  <Td isTruncated maxWidth="80px">
+                    {project.owner_account_name}
+                  </Td>
+                  <Td isTruncated maxWidth="150px">
+                    <Link
+                      as={RouterLink}
+                      to={`/${project.owner_account_name}/${project.name}`}
+                    >
+                      {project.title}
+                    </Link>
+                  </Td>
+                  <Td isTruncated maxWidth="150px">
+                    <Link href={project.git_repo_url} isExternal>
+                      <ExternalLinkIcon mx="2px" /> {project.git_repo_url}
+                    </Link>
+                  </Td>
+                  <Td
+                    color={!project.description ? "ui.dim" : "inherit"}
+                    isTruncated
+                    maxWidth="150px"
+                  >
+                    {project.description || "N/A"}
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          )}
+        </Table>
+      </TableContainer>
+      <Flex
+        gap={4}
+        alignItems="center"
+        mt={4}
+        direction="row"
+        justifyContent="flex-end"
+      >
+        <Button onClick={() => setPage(page - 1)} isDisabled={!hasPreviousPage}>
+          Previous
+        </Button>
+        <span>Page {page}</span>
+        <Button isDisabled={!hasNextPage} onClick={() => setPage(page + 1)}>
+          Next
+        </Button>
+      </Flex>
+    </>
+  )
+}
+
 function PublicProjects() {
   return (
     <Container maxW={pageWidthNoSidebar}>
-      <Heading size="lg" textAlign={{ base: "center", md: "left" }} pt={12}>
-        Public projects
+      <Heading
+        size="lg"
+        textAlign={{ base: "center", md: "left" }}
+        pt={12}
+        mb={4}
+      >
+        Browse all projects
       </Heading>
-      <Box pt={5}>Coming soon!</Box>
+      <PublicProjectsTable />
     </Container>
   )
 }
