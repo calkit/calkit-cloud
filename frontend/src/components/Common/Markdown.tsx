8d50174351e628a4820a4ba74f219eb698afd194
Pete Bachant <petebachant@gmail.com>
2024-12-17 04:53:06 -0800

    Reduce my for inline code

--- frontend/src/components/Common/Markdown.tsx
+++ frontend/src/components/Common/Markdown.tsx
@@ -48,7 +48,7 @@ const code = ({ insidePre = false, ...props }: codeProps) => {
   if (insidePre) {
     return <Code my={2} whiteSpace={"pre"} display={"block"} p={2} {...props} />
   } else {
-    return <Code my={2} whiteSpace={"pre"} px={1} {...props} />
+    return <Code my={0} whiteSpace={"pre"} px={1} {...props} />
   }
 }
 
