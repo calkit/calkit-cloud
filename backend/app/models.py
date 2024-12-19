0dae4748cb11e8f7056cde19e67488ddef4e5830
Pete Bachant <petebachant@gmail.com>
2024-12-19 08:32:29 -0800

    Restrict invalid user account names

--- backend/app/models.py
+++ backend/app/models.py
@@ -57,12 +57,14 @@ class UserBase(SQLModel):
 # Properties to receive via API on creation
 class UserCreate(UserBase):
     password: str = Field(min_length=8, max_length=40)
+    account_name: str | None = Field(default=None, max_length=64)
     github_username: str = Field(default=None, max_length=64)
 
 
 class UserRegister(SQLModel):
     email: EmailStr = Field(max_length=255)
     password: str = Field(min_length=8, max_length=40)
+    account_name: str | None = Field(default=None, max_length=64)
     full_name: str | None = Field(default=None, max_length=255)
 
 
