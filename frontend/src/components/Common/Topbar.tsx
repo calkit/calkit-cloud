f38f3acc14fe4d58e481a1d61e38b77e5a369c1b
Pete Bachant <petebachant@gmail.com>
2024-12-19 06:01:15 -0800

    Use logo with no background

--- frontend/src/components/Common/Topbar.tsx
+++ frontend/src/components/Common/Topbar.tsx
@@ -73,8 +73,8 @@ export default function Topbar() {
             <Box px={8}>
               <Link as={RouterLink} to="/">
                 <Image
-                  width={20}
-                  src="/assets/images/calkit.svg"
+                  width={"95px"}
+                  src="/assets/images/calkit-no-bg.svg"
                   alt="Calkit logo"
                 />
               </Link>
