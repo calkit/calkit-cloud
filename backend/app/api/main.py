9a847a9179964c6836b4c0ee9be3cc199a2a7374
Pete Bachant <petebachant@gmail.com>
2024-12-19 17:13:12 -0800

    Add API endpoint to fetch datasets

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,4 +1,4 @@
-from app.api.routes import login, misc, projects, users, orgs
+from app.api.routes import login, misc, projects, users, orgs, datasets
 from fastapi import APIRouter
 
 api_router = APIRouter()
@@ -7,3 +7,4 @@ api_router.include_router(users.router, tags=["users"])
 api_router.include_router(misc.router, tags=["misc"])
 api_router.include_router(projects.router, tags=["projects"])
 api_router.include_router(orgs.router, tags=["orgs"])
+api_router.include_router(datasets.router, tags=["datasets"])
---
+++ backend/app/api/routes/datasets.py
@@ -0,0 +1,64 @@
+"""API endpoints for datasets."""
+
+import logging
+
+import sqlalchemy
+from app.api.deps import CurrentUser, SessionDep
+from app.models import Dataset, Project, ProjectPublic
+from fastapi import APIRouter
+from sqlmodel import SQLModel, func, or_, select
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+router = APIRouter()
+
+
+class DatasetResponse(SQLModel):
+    project: ProjectPublic
+    path: str
+    title: str | None
+    description: str | None
+    imported_from: str | None
+
+
+class DatasetsResponse(SQLModel):
+    data: list[DatasetResponse]
+    count: int
+
+
+@router.get("/datasets")
+def get_datasets(
+    session: SessionDep,
+    current_user: CurrentUser,
+    limit: int = 100,
+    offset: int = 0,
+) -> DatasetsResponse:
+    # TODO: Handle collaborator access for private project datasets
+    count_query = (
+        select(func.count())
+        .select_from(Dataset)
+        .join(Project)
+        .where(
+            or_(
+                Project.is_public,
+                Project.owner_account_id == current_user.account.id,
+            )
+        )
+    )
+    count = session.exec(count_query).one()
+    select_query = (
+        select(Dataset)
+        .join(Project)
+        .where(
+            or_(
+                Project.is_public,
+                Project.owner_account_id == current_user.account.id,
+            )
+        )
+        .order_by(sqlalchemy.asc(Project.title))
+        .limit(limit)
+        .offset(offset)
+    )
+    datasets = session.exec(select_query).all()
+    return DatasetsResponse(data=datasets, count=count)
