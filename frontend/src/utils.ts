ac0fb5713cbe33b8b5185880895594141b065c4a
Pete Bachant <petebachant@gmail.com>
2025-02-17 08:33:23 -0800

    Update formatTimestamp function

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -81,13 +81,17 @@ export const capitalizeFirstLetter = (val: string) => {
 
 export const formatTimestamp = (isoString: string) => {
   const date = new Date(isoString)
-  return date.toLocaleString("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: true,
-  })
+  return (
+    date
+      .toLocaleString("en-CA", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+      })
+      .replace(/,/g, "") + " (UTC)"
+  )
 }
