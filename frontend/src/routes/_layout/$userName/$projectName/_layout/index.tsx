a3718ff1479598e2b7ec93adfd1a98042f94b684
Pete Bachant <petebachant@gmail.com>
2025-08-20 07:51:25 -0700

    Remove repro check; resolves #411

--- frontend/src/routes/_layout/$userName/$projectName/_layout/index.tsx
+++ frontend/src/routes/_layout/$userName/$projectName/_layout/index.tsx
@@ -16,11 +16,10 @@ import {
   IconButton,
   Link,
   Icon,
-  Code,
 } from "@chakra-ui/react"
 import { createFileRoute, Link as RouterLink } from "@tanstack/react-router"
 import { useState } from "react"
-import { FaPlus, FaSync } from "react-icons/fa"
+import { FaPlus } from "react-icons/fa"
 import { MdEdit } from "react-icons/md"
 import { ExternalLinkIcon } from "@chakra-ui/icons"
 
@@ -45,12 +44,10 @@ function ProjectView() {
   const secBgColor = useColorModeValue("ui.secondary", "ui.darkSlate")
   const { userName, projectName } = Route.useParams()
   const [showClosedTodos, setShowClosedTodos] = useState(false)
-  const {
-    projectRequest,
-    reproCheckRequest,
-    putDevcontainerMutation,
-    userHasWriteAccess,
-  } = useProject(userName, projectName)
+  const { projectRequest, userHasWriteAccess } = useProject(
+    userName,
+    projectName,
+  )
   const { issuesRequest, issueStateMutation } = useProjectIssues(
     userName,
     projectName,
@@ -58,7 +55,6 @@ function ProjectView() {
   )
   const { readmeRequest } = useProjectReadme(userName, projectName)
   const { questionsRequest } = useProjectQuestions(userName, projectName)
-  const reproCheck = reproCheckRequest.data
   const gitRepoUrl = projectRequest.data?.git_repo_url
   const codespacesUrl =
     String(gitRepoUrl).replace("://github.com/", "://codespaces.new/") +
@@ -260,157 +256,6 @@ function ProjectView() {
                     </Text>
                   </Flex>
                 ))}
-              </>
-            )}
-          </Box>
-          {/* Reproducibility check */}
-          <Box py={4} px={6} mb={4} borderRadius="lg" bg={secBgColor}>
-            <Flex>
-              <Heading size="md" mb={2}>
-                Reproducibility check
-              </Heading>
-              <IconButton
-                aria-label="Refresh repro check"
-                height="25px"
-                width="28px"
-                ml={1.5}
-                icon={<FaSync />}
-                size={"xs"}
-                onClick={() => reproCheckRequest.refetch()}
-              />
-            </Flex>
-            {reproCheckRequest.isPending ||
-            reproCheckRequest.isRefetching ||
-            putDevcontainerMutation.isPending ? (
-              <Flex justify="center" align="center" height="100px" width="full">
-                <Spinner size="xl" color="ui.main" />
-              </Flex>
-            ) : (
-              <>
-                <Text>
-                  Has README.md: {reproCheck?.has_readme ? "✅" : "❌"}
-                </Text>
-                <Text>
-                  README.md has instructions:{" "}
-                  {reproCheck?.instructions_in_readme ? "✅" : "❌"}
-                </Text>
-                <Text>
-                  DVC initialized: {reproCheck?.is_dvc_repo ? "✅" : "❌"}
-                </Text>
-                <Text>
-                  DVC remote defined: {reproCheck?.n_dvc_remotes ? "✅" : "❌"}
-                </Text>
-                <Text>
-                  Has pipeline (<Code>dvc.yaml</Code>):{" "}
-                  {reproCheck?.has_pipeline ? "✅" : "❌"}
-                </Text>
-                <Text>
-                  Has Calkit metadata (<Code>calkit.yaml</Code>):{" "}
-                  {reproCheck?.has_calkit_info ? "✅" : "❌"}
-                </Text>
-                <Text>
-                  Has dev container spec:{" "}
-                  {reproCheck?.has_dev_container ? (
-                    "✅"
-                  ) : (
-                    <>
-                      {"❌ "}
-                      {userHasWriteAccess ? (
-                        <Link onClick={() => putDevcontainerMutation.mutate()}>
-                          🔧
-                        </Link>
-                      ) : (
-                        ""
-                      )}
-                    </>
-                  )}
-                </Text>
-                <Text>
-                  Environments defined:{" "}
-                  {reproCheck ? (
-                    <>
-                      {reproCheck.n_environments}{" "}
-                      {reproCheck.n_environments ? "✅" : "❌"}
-                    </>
-                  ) : (
-                    ""
-                  )}
-                </Text>
-                <Text>
-                  Pipeline stages run in an environment:{" "}
-                  {reproCheck ? (
-                    <>
-                      {reproCheck.n_stages_with_env}/{reproCheck.n_stages}{" "}
-                      {reproCheck.n_stages_without_env ? "❌" : "✅"}
-                    </>
-                  ) : (
-                    ""
-                  )}
-                </Text>
-                <Text>
-                  Datasets imported or created by pipeline:{" "}
-                  {reproCheck ? (
-                    <>
-                      {reproCheck.n_datasets_with_import_or_stage}/
-                      {reproCheck.n_datasets}{" "}
-                      {reproCheck.n_datasets_no_import_or_stage ? "❌" : "✅"}
-                    </>
-                  ) : (
-                    ""
-                  )}
-                </Text>
-                <Text>
-                  Figures imported or created by pipeline:{" "}
-                  {reproCheck ? (
-                    <>
-                      {reproCheck.n_figures_with_import_or_stage}/
-                      {reproCheck.n_figures}{" "}
-                      {reproCheck.n_figures_no_import_or_stage ? "❌" : "✅"}
-                    </>
-                  ) : (
-                    ""
-                  )}
-                </Text>
-                <Text>
-                  Publications imported or created by pipeline:{" "}
-                  {reproCheck ? (
-                    <>
-                      {reproCheck.n_publications_with_import_or_stage}/
-                      {reproCheck.n_publications}{" "}
-                      {reproCheck.n_publications_no_import_or_stage
-                        ? "❌"
-                        : "✅"}
-                    </>
-                  ) : (
-                    ""
-                  )}
-                </Text>
-                {userHasWriteAccess ? (
-                  <>
-                    <Heading
-                      size="sm"
-                      mt={4}
-                      mb={-2}
-                      color={
-                        reproCheck?.recommendation ? "yellow.500" : "green.500"
-                      }
-                    >
-                      Recommendation
-                    </Heading>
-                    {reproCheck?.recommendation ? (
-                      <>
-                        <Markdown>{reproCheck.recommendation}</Markdown>
-                      </>
-                    ) : (
-                      <Markdown>
-                        This project looks good from here! Check in depth
-                        locally with `calkit status` and `calkit run`.
-                      </Markdown>
-                    )}
-                  </>
-                ) : (
-                  ""
-                )}
               </>
             )}
           </Box>
