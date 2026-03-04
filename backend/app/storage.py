e1d14ff84aab6a2b09c1e2e5278bf37d8534481b
Pete Bachant <petebachant@gmail.com>
2026-03-03 17:47:43 -0800

    Move into function

--- backend/app/storage.py
+++ backend/app/storage.py
@@ -318,3 +318,36 @@ def get_storage_usage(
     if isinstance(usage, dict):
         usage = sum(float(v) for v in usage.values())
     return float(usage) / 1e9
+
+
+def migrate_legacy_dvc_paths(dry_run=True):
+    """Migrate legacy DVC paths in object storage to new structure with
+    'files/md5/'.
+    """
+    fs = get_object_fs()
+    data_prefix = get_data_prefix()
+    # Iterate over all owners and projects, renaming any two character folders
+    # to prepend 'files/md5/' to match the new DVC path structure
+    for owner_path in fs.ls(data_prefix, False):
+        owner_name = os.path.basename(owner_path)
+        owner_prefix = get_data_prefix_for_owner(owner_name)
+        for project_path in fs.ls(owner_prefix, False):
+            project_name = os.path.basename(project_path)
+            project_prefix = f"{owner_prefix}/{project_name}"
+            for item_path in fs.ls(project_prefix, False):
+                item_name = os.path.basename(item_path)
+                if len(item_name) == 2 and all(c.isalnum() for c in item_name):
+                    old_path = (
+                        f"{data_prefix}/{owner_name}/{project_name}"
+                        f"/{item_name}"
+                    )
+                    new_path = (
+                        f"{data_prefix}/{owner_name}/{project_name}"
+                        f"/files/md5/{item_name}"
+                    )
+                    print(f"Renaming {old_path} to {new_path}")
+                    if not dry_run:
+                        try:
+                            fs.rename(old_path, new_path)
+                        except Exception as e:
+                            print(f"Error renaming {old_path}: {e}")
