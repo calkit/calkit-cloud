2f81950bbba720d113bd866a7d99e4fd17a01bfa
Pete Bachant <petebachant@gmail.com>
2026-04-08 13:14:38 -0700

    Store playwright tests results on failure

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
 
 
 /**
@@ -29,6 +29,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    /* Record video on failed tests */
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
@@ -89,4 +92,4 @@ export default defineConfig({
     url: 'http://localhost:5173',
     reuseExistingServer: true,
   },
-});
+})
